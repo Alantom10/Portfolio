10ac6e600bd7967389737476e9ab7bfd66f0bccf
acthomas <alan.thomas@tsworkz.com>
2023-05-07 16:44:20 -0700

    Minor fixes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,23 @@ import './css/navbar.css';
 export default class Navbar extends Component {
     state = { 
         active: 'nav__menu',
-     } 
+    }
+
+    navStyle = {
+        textDecoration:'none',
+        fontFamily: "'Chathura', sans-serif",
+        fontSize:'200%',
+        color: 'white',
+        borderRadius: '5px',
+        animation: '0.5s linear 0s 1 normal forwards running bgColorChangeBack',
+        paddingBottom: '8px',
+        paddingLeft: '24px',
+        paddingRight: '24px'
+    };
+
+    navMenuStyle = {
+        gap: '0px'
+    }
 
     navToggle = () => {
         let newActive = 'nav__menu';
@@ -17,13 +33,13 @@ export default class Navbar extends Component {
         return (
             <nav className='nav'>
                 <a href='/' className='nav__logo'>Alan</a>
-                <ul className={this.state.active}>
-                    <li className="nav__link__box"><a href="/" className="nav__link">Home</a></li>
-                    <li className="nav__link__box"><a href="/about_me" className="nav__link">About Me</a></li>
-                    <li className="nav__link__box"><a href="/resume" className="nav__link">Resume</a></li>
-                    <li className="nav__link__box"><a href="/skills" className="nav__link">Skills</a></li>
-                    <li className="nav__link__box"><a href="/projects" className="nav__link">Projects</a></li>
-                    <li className="nav__link__box"><a href="/contacts" className="nav__link">Contacts</a></li>
+                <ul className={this.state.active} style={this.navMenuStyle}>
+                    <li className="nav__link__box"><a style={this.navStyle} href="/" className="nav__link">Home</a></li>
+                    <li className="nav__link__box"><a style={this.navStyle} href="/about_me" className="nav__link">About Me</a></li>
+                    <li className="nav__link__box"><a style={this.navStyle} href="/resume" className="nav__link">Resume</a></li>
+                    <li className="nav__link__box"><a style={this.navStyle} href="/skills" className="nav__link">Skills</a></li>
+                    <li className="nav__link__box"><a style={this.navStyle} href="/projects" className="nav__link">Projects</a></li>
+                    <li className="nav__link__box"><a style={this.navStyle} href="/contacts" className="nav__link">Contacts</a></li>
                 </ul>
                 <div onClick = {() => this.navToggle()} className="nav__toggler">
                     <div className="line1"></div>
